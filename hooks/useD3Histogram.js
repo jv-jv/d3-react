2169c586345ef4076b651d517c1c48e667e09dc3
jv-jv <jacopo.v@pm.me>
2021-04-10 18:19:31 +0100

    d3 boilerplate

---
+++ hooks/useD3Histogram.js
@@ -0,0 +1,26 @@
+import { useEffect, useRef } from "react"
+
+export default function useD3Histogram() {
+  const divRef = useRef()
+
+  useEffect(() => {
+    // 1. Access Data
+
+    // 2. Create Dimensions
+
+    // 3. Draw Canvas
+
+    // 4. Create Scales
+
+    // 5. Draw Data
+
+    // 6. Draw Peripherals
+
+    // 7. Set Up Interactions
+
+    // clean up
+    return () => {}
+  })
+
+  return divRef
+}
